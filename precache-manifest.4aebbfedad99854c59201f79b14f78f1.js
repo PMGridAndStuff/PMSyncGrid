526158587d32f48e60ba29f7420dae111a0f89d3
PMGridAndStuff <61727423+PMGridAndStuff@users.noreply.github.com>
2020-03-03 04:13:08 -0500

    Updates

---
+++ precache-manifest.4aebbfedad99854c59201f79b14f78f1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e429022e71ac1c70ac4da3152d227b52",
+    "url": "/PMSyncGrid/index.html"
+  },
+  {
+    "revision": "465983b6a339f702abdb",
+    "url": "/PMSyncGrid/static/css/main.d8962b16.chunk.css"
+  },
+  {
+    "revision": "b4bb273b9a4dd7f0aafd",
+    "url": "/PMSyncGrid/static/js/2.2ece069e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PMSyncGrid/static/js/2.2ece069e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465983b6a339f702abdb",
+    "url": "/PMSyncGrid/static/js/main.0411d9d9.chunk.js"
+  },
+  {
+    "revision": "f44ab35eaa768ccf6019",
+    "url": "/PMSyncGrid/static/js/runtime-main.9c203cc6.js"
+  },
+  {
+    "revision": "2b574d926c1059d6a5084cc91c48ac81",
+    "url": "/PMSyncGrid/static/media/background1.2b574d92.png"
+  },
+  {
+    "revision": "0e81b86060bbcdb8ca097171d96e0af5",
+    "url": "/PMSyncGrid/static/media/center.0e81b860.png"
+  },
+  {
+    "revision": "63079b5a518e27ceddae1cfd5f692a31",
+    "url": "/PMSyncGrid/static/media/ch0000_00_red_256.ktx.63079b5a.png"
+  },
+  {
+    "revision": "e0953707ccdfdf6423ff54b82ba41018",
+    "url": "/PMSyncGrid/static/media/ch0048_00_gima_256.ktx.e0953707.png"
+  },
+  {
+    "revision": "47b7f971e7f75f9d7dd3878ecdf15b8b",
+    "url": "/PMSyncGrid/static/media/ch0062_00_karin_256.ktx.47b7f971.png"
+  },
+  {
+    "revision": "696e7200110474dcf1251514ff5fa653",
+    "url": "/PMSyncGrid/static/media/pm0006_00_lizardon_256.ktx.696e7200.png"
+  },
+  {
+    "revision": "91c04c5c894f1af01b73a44602c7f89a",
+    "url": "/PMSyncGrid/static/media/pm0229_01_hellgar_256.ktx.91c04c5c.png"
+  },
+  {
+    "revision": "56905c2142e6d79da6deb0eef89d047c",
+    "url": "/PMSyncGrid/static/media/pm0510_00_lepardas_256.ktx.56905c21.png"
+  },
+  {
+    "revision": "0ad87fa4f8ac6d705c5a38a7a8083200",
+    "url": "/PMSyncGrid/static/media/selected.0ad87fa4.png"
+  }
+]);
